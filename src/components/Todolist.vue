f8fb2751351e0a0baba40420e8771c4420c46bc9
Réau <anaisreau@gmail.com>
2021-02-19 14:43:00 +0100

    adding count of incompleted task ans clear all

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,23 +1,33 @@
 <template>
-<div class="content">
-    To Do List Vue.js 
+  <div class="content">
+    To Do List Vue.js
     <div>
-        <br/>
-        <input class='input' type="text" placeholder="enter your task here" v-model="newTask" @keyup.enter="addTask">
-        
-    </div><br/>
-            <a v-for="task in tasks" :key="task.id" class="task" :class="{completed : task.completed}">   
-            <div class="todos">   
-            <input type="checkbox" v-model="task.completed" class="toggle">
-            <label>{{ task.title }}</label>
-            </div>    
-            </a>
-        </div>
-
-
+      <div>
+        <h4 v-if="incomplete">You still have {{ incomplete }} task to do</h4>
+      </div>
+      <input
+        class="input"
+        type="text"
+        placeholder="enter your task here"
+        v-model="newTask"
+        @keyup.enter="addTask"
+      />
+    </div>
+    <br />
+    <a
+      v-for="task in tasks"
+      :key="task.id"
+      class="task"
+      :class="{ completed: task.completed }"
+    >
+      <div class="todos">
+        <input type="checkbox" v-model="task.completed" class="toggle" />
+        <label>{{ task.title }}</label>
+      </div>
+    </a>
+    <button @click="clearAll">Clear All Task</button>
+  </div>
 </template>
-
-
 
 <style>
 .completed {
@@ -27,61 +37,65 @@
   margin: auto;
 }
 
-.content{
-    font-size: 2em;
-    }
+.content {
+  font-size: 2em;
+}
 
-.todos{
-    text-align: left;
-    margin-left: 30%;
-    }
+.todos {
+  text-align: left;
+  margin-left: 30%;
+}
 
-.toggle{
-    margin-right: 1.5em;
-    width:30px;
-	height:30px;
-    }
+.toggle {
+  margin-right: 1.5em;
+  width: 30px;
+  height: 30px;
+}
 
-.input{
-    width:40%;
-    font-size : 1em
-     }
-
+.input {
+  width: 40%;
+  font-size: 1em;
+}
 </style>
-
 
 <script>
 export default {
-    name : "todoList",
-    data() {
-       
-        return {
-            newTask : "",
-            idTask : 2,
-            tasks : [{
-                "id" : 1 , 
-                "title" : "first test",
-                "completed" : false
-}
-            ]
-            }
-},
+  name: "todoList",
+  data() {
+    return {
+      newTask: "",
+      idTask: [],
+      incompleted: [],
+      tasks: [{}],
+    };
+  },
 
+  computed: {
+    incomplete() {
+      return this.tasks.filter(this.notCompleted).length;
+    },
+  },
 
-methods:{
+  methods: {
     addTask() {
+      if (this.newTask) {
         this.tasks.push({
-        id : this.idTask,
-        title : this.newTask,
-        completed : false 
-    })
+          id: this.idTask,
+          title: this.newTask,
+          completed: false,
+        });
+        (this.newTask = ""), this.idTask++;
+      }
+    },
 
-this.newTask = "",
-this.idTask++
-    
-}, 
+    clearAll() {
+      this.tasks = [];
+      this.idTask = 0;
+    },
 
-}
-}
-
+    notCompleted(task) {
+      return !task.completed;
+    },
+  },
+};
 </script>
